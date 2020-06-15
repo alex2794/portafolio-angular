32b9fb7ff2bfa87b953abf9ef453b9e98b258712
Enmanuel Alfaro <enmanuel.alfaror@gmail.com>
2020-06-15 14:18:02 -0600

    template sin rutas

---
+++ src/app/pages/portafolio/portafolio.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portafolio',
+  templateUrl: './portafolio.component.html',
+  styleUrls: ['./portafolio.component.css']
+})
+export class PortafolioComponent implements OnInit{
+  constructor() {}
+  ngOnInit() {
+
+  }
+}
